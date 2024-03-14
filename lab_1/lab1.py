5353c899a52e4fdcc1becf95616e7dc65749e653
Влад Жирнов <vlad-zhirnov-1995@mail.ru>
2024-03-14 17:04:34 +0400

    Exception handling has been added, the alphabet variable has been made a constant, paths are passed through the command line

--- lab_1/lab1.py
+++ lab_1/lab1.py
@@ -1,45 +1,45 @@
 import json
+import sys
+ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-def encryption_by_substitution():
-    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    with open('Information-Security/lab_1/encryption_key1.json') as f:
-        templates = json.load(f)
+def encryption_or_decryption_by_substitution(encryption_key1_file, input_file, mode):
+    try:
+        with open(encryption_key1_file) as f:
+            templates = json.load(f)
+    except FileNotFoundError:
+        print(f"Error: File {encryption_key1_file} not found!")
+
     for section, commands in templates.items():
         step = int(('\n'.join(commands)))
-    with open("Information-Security/lab_1/original1.txt") as file:
-        original = file.read().upper()
-    result = ""
-    for i in original:
-        place = alphabet.find(i)
-        new_place = place + step
-        if i in alphabet:
-            result += alphabet[new_place] 
+    try:
+        with open(input_file) as file:
+            data = file.read().upper()
+    except FileNotFoundError:
+        print(f"Error: {input_file} File not found!")
+        return
+    output = ""
+    for i in data:
+        place = ALPHABET.find(i)
+        new_place = None
+        match mode:
+            case 'encryption':
+                new_place = place + step
+            case 'decryption':
+                new_place = place - step
+        if i in ALPHABET:
+            output += ALPHABET[new_place] 
         else:
-            result += i
-    print(result)
+            output += i
+    print(output)
 
-def decryption_by_substitution():
-    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    with open('Information-Security/lab_1/encryption_key1.json') as f:
-        templates = json.load(f)
-    for section, commands in templates.items():
-        step = int(('\n'.join(commands)))
-    with open("Information-Security/lab_1/result1.txt") as file:
-        result = file.read().upper()
-    original = ""
-    for i in result:
-        place = alphabet.find(i)
-        new_place = place - step
-        if i in alphabet:
-            original += alphabet[new_place] 
-        else:
-            original += i
-    print(original)
-
-def check_character_frequency():
-    with open("Information-Security/lab_1/original_cod9.txt") as file:
-        original = file.read().upper()
-    count = len(original) - 10
+def check_character_frequency(original_cod9_file):
+    try:
+        with open(original_cod9_file) as file:
+            original = file.read().upper()
+    except FileNotFoundError:
+        print(f"Error: File {original_cod9_file} not found!")
+        return
+    count = len(original)
     arr_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-/;<=>?@[\]^_`"
     for i in arr_letters:
         if i in original:
@@ -47,10 +47,18 @@ def check_character_frequency():
             print(f"{i}({count_symbol}) : {count_symbol / count}")
 
 def decryption_by_frequency_analysis(filename, replacements_file):
-    with open(replacements_file, "r", encoding="utf-8") as json_file:
-        replacements = json.load(json_file)
-    with open(filename, "r", encoding="utf-8") as file:
-        encoded_text = file.read().upper()
+    try:
+        with open(replacements_file, "r", encoding="utf-8") as json_file:
+            replacements = json.load(json_file)
+    except FileNotFoundError:
+        print(f"Error: File {replacements_file} not found!")
+        return
+    try:
+        with open(filename, "r", encoding="utf-8") as file:
+            encoded_text = file.read().upper()
+    except FileNotFoundError:
+        print(f"Error: File {filename} not found!")
+        return
     decoded_text = ""
     for char in encoded_text:
         decoded_char = replacements.get(char, char)
@@ -58,8 +66,13 @@ def decryption_by_frequency_analysis(filename, replacements_file):
     print(decoded_text)
 
 if __name__ == "__main__":
-    decryption_by_substitution()
-    encryption_by_substitution()
-    check_character_frequency()
-    decryption_by_frequency_analysis("Information-Security/lab_1/original_cod9.txt", "Information-Security/lab_1/replacements.json")
+    encryption_key1_file = sys.argv[1]
+    input_file = sys.argv[2]
+    mode = sys.argv[3]
+    original_cod9_file = sys.argv[4]
+    replacements_file = sys.argv[5]
+
+    encryption_or_decryption_by_substitution(encryption_key1_file, input_file, mode)
+    check_character_frequency(original_cod9_file)
+    decryption_by_frequency_analysis(original_cod9_file, replacements_file)
     
