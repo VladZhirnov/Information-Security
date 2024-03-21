ac84f2113e93ecabc88d93e86b159949810c8988
Влад Жирнов <vlad-zhirnov-1995@mail.ru>
2024-03-21 19:48:22 +0400

    Code fixed

--- lab_2/java_generator.java
+++ lab_2/java_generator.java
@@ -1,7 +1,12 @@
 import java.util.Random;
 
+/**
+ * Класс для генерации случайных битов.
+ */
 public class RandomBitGenerator {
+    /* Максимальный размер генерируемой последовательности битов. */
     private static final int MAXSIZE = 128;
+    /* Генерирует случайные биты и выводит их на экран. */
     public void generateRandomBits() {
         Random random = new Random();
         for (int i = 0; i < MAXSIZE; i++)
@@ -9,7 +14,9 @@ public class RandomBitGenerator {
     }
 
     public static void main(String[] args) {
+        /* Создаем объект генератора случайных битов */
         RandomBitGenerator generator = new RandomBitGenerator();
+        /* Генерируем и выводим случайные биты */
         generator.generateRandomBits();
     }
 }
--- lab_2/lab2.py
+++ lab_2/lab2.py
@@ -8,30 +8,32 @@ import scipy
 pi = [0.2148, 0.3672, 0.2305, 0.1875]
 
 
-def frequency_bit_test(bits_row):
+def frequency_bit_test(bits_row, n):
     try:
-        x = None
         s = 0
         for bit in bits_row:
             if bit == "1":
                 x = 1
             else:
                 x = -1
-            s += (1 / pow(128, 0.5)) * x
+            s += (1 / pow(n, 0.5)) * x
         p = math.erfc(s / pow(2, 0.5))
         return p
     except Exception as e:
         print(f"Frequency bit test error: {str(e)}")
         
 
-def identical_bits_test(bits_row):
+def identical_bits_test(bits_row, n):
     try:
         z = 0
         for bit in bits_row:
-            z  += (1 / 128) * int(bit)
-        if abs(z - 1/2) < (2 / pow(128, 0.5)):
-            v = sum(bits_row[i] == bits_row[i + 1] for i in range(len(bits_row) - 1))
-            p = math.erfc((abs(v - 2 * 128 * z * (1 - z))) / (2 * pow(2 * 128, 0.5) * z * (1 - z)))
+            z  += (1 / n) * int(bit)
+        if abs(z - 1 / 2) < (2 / pow(n, 0.5)):
+            v = 0
+            for i in range(0, n - 1):
+                if bits_row[i] != bits_row[i + 1]:
+                    v += 1
+            p = math.erfc((abs(v - 2 * n * z * (1 - z))) / (2 * pow(2 * n, 0.5) * z * (1 - z)))
         else:
             p = 0
         return p
@@ -43,6 +45,7 @@ def sequence_of_units_test(bits_row):
     try:
         v1 = v2 = v3 = v4 = 0
         divided_bits = re.findall(r'.{%s}' % 8, bits_row)
+        print(divided_bits)
         for row in divided_bits:
             k = 0
             max_k = 0
@@ -53,15 +56,19 @@ def sequence_of_units_test(bits_row):
                     max_k = k
                 if bit == "0":
                     k = 0
-            if max_k == 1 or max_k < 1:
-                v1 += 1
-            elif max_k == 2:
-                v2 += 1
-            elif max_k == 3:
-                v3 += 1
-            else:
-                v4 += 1
+            match max_k:
+                case 0:
+                    v1 += 1
+                case 1:
+                    v1 += 1
+                case 2:
+                    v2 += 1
+                case 3:
+                    v3 += 1
+                case _:
+                    v4 += 1
         v = [v1, v2, v3, v4]
+        for i in v: print(i)
         x_square = 0
         for i in range(4):
             x_square += (pow(v[i] - 16 * pi[i], 2)) / (16 * pi[i])
@@ -72,24 +79,25 @@ def sequence_of_units_test(bits_row):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description=" ")
-    parser.add_argument("random_bits_json_file", help="Path to the input file")
-    args = parser.parse_args()
     try:
+        parser = argparse.ArgumentParser(description=" ")
+        parser.add_argument("random_bits_json_file", help="Path to the input file")
+        parser.add_argument("n", type = int, help="bit string length")
+        args = parser.parse_args()
         with open(args.random_bits_json_file, "r", encoding="utf-8") as f:
             bits_row = json.load(f)
-    except Exception as e:
-         print(f"Error with {args.random_bits_json_file} file: {str(e)}")
-    cpp_row = bits_row["c++"]
-    java_row = bits_row["java"]
-    with open(bits_row["path"], mode = "w", encoding = "utf-8") as results:
-        results.write("NIST TEST RESULTS:\n")
-        results.write("C++:\n")
-        results.write(f"1) Frequency bit test: P = {frequency_bit_test(cpp_row)}\n")
-        results.write(f"2) Identical bits test: P = {identical_bits_test(cpp_row)}\n")
-        results.write(f"3) Sequence of units test: P = {sequence_of_units_test(cpp_row)}\n")
+        cpp_row = bits_row["c++"]
+        java_row = bits_row["java"]
+        with open(bits_row["path"], mode = "w", encoding = "utf-8") as results:
+            results.write("NIST TEST RESULTS:\n")
+            results.write("C++:\n")
+            results.write(f"1) Frequency bit test: P = {frequency_bit_test(cpp_row, args.n)}\n")
+            results.write(f"2) Identical bits test: P = {identical_bits_test(cpp_row, args.n)}\n")
+            results.write(f"3) Sequence of units test: P = {sequence_of_units_test(cpp_row)}\n")
 
-        results.write("Java:\n")
-        results.write(f"1) Frequency bit test: P = {frequency_bit_test(java_row)}\n")
-        results.write(f"2) Identical bits test: P = {identical_bits_test(java_row)}\n")
-        results.write(f"3) Sequence of units test: P = {sequence_of_units_test(java_row)}\n")
+            results.write("Java:\n")
+            results.write(f"1) Frequency bit test: P = {frequency_bit_test(java_row, args.n)}\n")
+            results.write(f"2) Identical bits test: P = {identical_bits_test(java_row, args.n)}\n")
+            results.write(f"3) Sequence of units test: P = {sequence_of_units_test(java_row)}\n")
+    except Exception as e:
+         print(f"Error with code in main: {str(e)}")
