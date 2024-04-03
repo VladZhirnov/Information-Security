7bf74d5c2e6e439c6a109164c2a4e4336791cee7
Влад Жирнов <vlad-zhirnov-1995@mail.ru>
2024-04-03 20:20:24 +0400

    Code fixed 3

--- lab_2/lab2.py
+++ lab_2/lab2.py
@@ -8,32 +8,33 @@ import scipy
 pi = [0.2148, 0.3672, 0.2305, 0.1875]
 
 
-def frequency_bit_test(bits_row, n):
+def frequency_bit_test(bits_row):
     try:
+        length = len(bits_row)
         s = 0
         for bit in bits_row:
             if bit == "1":
                 x = 1
             else:
                 x = -1
-            s += (1 / pow(n, 0.5)) * x
+            s += (1 / pow(length, 0.5)) * x
         p = math.erfc(s / pow(2, 0.5))
         return p
     except Exception as e:
         print(f"Frequency bit test error: {str(e)}")
         
 
-def identical_bits_test(bits_row, n):
+def identical_bits_test(bits_row):
     try:
-        z = 0
-        for bit in bits_row:
-            z  += (1 / n) * int(bit)
-        if abs(z - 1 / 2) < (2 / pow(n, 0.5)):
+        length = len(bits_row)
+        count_1 = bits_row.count('1')
+        z = (1 / length) * count_1
+        if abs(z - 1 / 2) < (2 / pow(length, 0.5)):
             v = 0
-            for i in range(0, n - 1):
+            for i in range(0, length - 1):
                 if bits_row[i] != bits_row[i + 1]:
                     v += 1
-            p = math.erfc((abs(v - 2 * n * z * (1 - z))) / (2 * pow(2 * n, 0.5) * z * (1 - z)))
+            p = math.erfc((abs(v - 2 * length * z * (1 - z))) / (2 * pow(2 * length, 0.5) * z * (1 - z)))
         else:
             p = 0
         return p
@@ -80,7 +81,6 @@ if __name__ == "__main__":
     try:
         parser = argparse.ArgumentParser(description=" ")
         parser.add_argument("random_bits_json_file", help="Path to the input file")
-        parser.add_argument("n", type = int, help="bit string length")
         args = parser.parse_args()
         with open(args.random_bits_json_file, "r", encoding="utf-8") as f:
             bits_row = json.load(f)
@@ -89,13 +89,13 @@ if __name__ == "__main__":
         with open(bits_row["path"], mode = "w", encoding = "utf-8") as results:
             results.write("NIST TEST RESULTS:\n")
             results.write("C++:\n")
-            results.write(f"1) Frequency bit test: P = {frequency_bit_test(cpp_row, args.n)}\n")
-            results.write(f"2) Identical bits test: P = {identical_bits_test(cpp_row, args.n)}\n")
+            results.write(f"1) Frequency bit test: P = {frequency_bit_test(cpp_row)}\n")
+            results.write(f"2) Identical bits test: P = {identical_bits_test(cpp_row)}\n")
             results.write(f"3) Sequence of units test: P = {sequence_of_units_test(cpp_row)}\n")
 
             results.write("Java:\n")
-            results.write(f"1) Frequency bit test: P = {frequency_bit_test(java_row, args.n)}\n")
-            results.write(f"2) Identical bits test: P = {identical_bits_test(java_row, args.n)}\n")
+            results.write(f"1) Frequency bit test: P = {frequency_bit_test(java_row)}\n")
+            results.write(f"2) Identical bits test: P = {identical_bits_test(java_row)}\n")
             results.write(f"3) Sequence of units test: P = {sequence_of_units_test(java_row)}\n")
     except Exception as e:
          print(f"Error with code in main: {str(e)}")
